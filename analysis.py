72efead28cbea1ae785469947425d03cda5d20a4
Oskar <oskar.grusiecki@gmail.com>
2026-05-19 19:04:34 +0200

    data cleaning: remove country nulls

--- analysis.py
+++ analysis.py
@@ -8,8 +8,10 @@ print("Wymiary:", df.shape)
 print("\nKolumny:", df.columns.tolist())
 print("\nPuste wartości:\n", df.isnull().sum())
 
+
 # Ile filmow vs seriali?
 print("\nTypy treści:\n", df['type'].value_counts())
+
 
 # Top 10 krajów
 print("\nTop 10 krajów:\n", df['country'].value_counts().head(10))
@@ -19,7 +21,11 @@ nowe = df[df['release_year'] > 2019]
 print("\nFilmy po 2019:", len(nowe))
 
 conn = sqlite3.connect('netflix.db')
-df.to_sql('shows', conn, if_exists='replace', index=False)
+
+df_clean = df.dropna(subset=["country"])
+df_clean.to_sql('shows', conn, if_exists='replace', index=False)
+
+
 
 def run_query(conn,query,title):
 	result = conn.execute(query)
@@ -30,7 +36,7 @@ run_query(conn,"SELECT type, COUNT(*) as count FROM shows GROUP BY type", "Filmy
 
 run_query(conn,"SELECT title, release_year FROM shows WHERE country = 'United States' AND release_year >= 2015", "US Shows after 2015")
 
-run_query(conn, "SELECT country, COUNT(*) as top FROM shows WHERE country IS NOT NULL GROUP BY country ORDER BY top DESC LIMIT 5", "Top countries")
+run_query(conn, "SELECT country, COUNT(*) as top FROM shows GROUP BY country ORDER BY top DESC LIMIT 5", "Top countries")
 
 
 conn.close()   
